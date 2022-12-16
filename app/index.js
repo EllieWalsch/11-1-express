40a9fda2146faa1929cb8199b625b70f99a0d091
EllieWalsch <elliewalsch4@gmail.com>
2022-12-15 19:47:51 -0600

    Send back imported JSON data

--- app/index.js
+++ app/index.js
@@ -1,21 +1,13 @@
 import express from "express";
+import terms from "./db/terms.json" assert { type: "json" };
 
 // Start express
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send(
-    `<p>API - An application programming interface, is a computing interface that defines interactions between multiple software intermediaries</p>`
-  );
-});
-
-app.get("/api", (req, res) => {
-  res.json({
-    term: "api",
-    description:
-      "An application programming interface, is a computing interface that defines interactions between multiple software intermediaries",
-  });
+// gets file from a "database"
+app.get("/api", (_, res) => {
+  res.json(terms);
 });
 
 // start the server
