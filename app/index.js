704cf6e3b9ac7e37409e811cb17a8586cbea3034
EllieWalsch <elliewalsch4@gmail.com>
2022-12-15 20:21:57 -0600

    Add example of dynamic param

--- app/index.js
+++ app/index.js
@@ -1,13 +1,39 @@
 import express from "express";
-import terms from "./db/terms.json" assert { type: "json" };
+import termsData from "./db/terms.json" assert { type: "json" };
 
 // Start express
 const app = express();
 const port = 3001;
 
 // gets file from a "database"
-app.get("/api", (_, res) => {
-  res.json(terms);
+app.get("/api/terms", (_, res) => {
+  res.json(termsData);
+});
+
+// ":term" means route can be whatever the user wants
+// the colon represents a dynamic parameter
+// eg: /api/terms/whateverIwant
+
+// what follows the : (the parameter) is what will show up as the key in the
+// request.params
+
+app.get("/api/terms/:term", (req, res) => {
+  // get the desired term from the req.params
+  const { term } = req.params;
+
+  // find the desired term in the array
+  const requestedTerm = termsData.find(
+    (t) => t.term.toUpperCase() === term.toUpperCase()
+  );
+
+  // if we find the term, send it back as JSON
+  // else, tell them we did not find it
+  // 404 mean user made a bad request
+  if (requestedTerm) {
+    res.json(requestedTerm);
+  } else {
+    res.status(404).json({ error: `Term ${term} not found. :(` });
+  }
 });
 
 // start the server
